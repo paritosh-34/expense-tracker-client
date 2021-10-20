8067c5ce8233229d49364db57361d696edc2d655
Paritosh Batish <batishparitosh2@gmail.com>
2021-10-21 02:48:39 +0530

    One password message at a time

--- src/utils/validatePassword.ts
+++ src/utils/validatePassword.ts
@@ -5,43 +5,21 @@ const validatePassword = (candidatePassword: string): [boolean, string] => {
   const search3 = candidatePassword.search(/[0-9]/);
   const search4 = candidatePassword.search(/[@#$%^&]/);
 
-  let isErr = false;
-  const errors: string[] = [];
-
-  if (length < 8) {
-    isErr = true;
-    errors.push('Password must be atleast 8 characters.');
-  }
-  if (search1 < 0) {
-    isErr = true;
-    errors.push('Password must contain atleast one lowercase letter.');
-  }
-  if (search2 < 0) {
-    isErr = true;
-    errors.push('Password must contain atleast one uppercase letter.');
-  }
-  if (search3 < 0) {
-    isErr = true;
-    errors.push('Password must contain atleast one number.');
-  }
-  if (search4 < 0) {
-    isErr = true;
-    errors.push('Password must contain atleast one special character.');
-  }
+  if (length < 8) return [true, 'Password must be atleast 8 characters.'];
+  if (search1 < 0) return [true, 'Password must contain atleast one lowercase letter.'];
+  if (search2 < 0) return [true, 'Password must contain atleast one uppercase letter.'];
+  if (search3 < 0) return [true, 'Password must contain atleast one number.'];
+  if (search4 < 0) return [true, 'Password must contain atleast one special character.'];
 
   const s1 = (candidatePassword.match(/[a-z]/g) || []).length;
   const s2 = (candidatePassword.match(/[A-Z]/g) || []).length;
   const s3 = (candidatePassword.match(/[0-9]/g) || []).length;
   const s4 = (candidatePassword.match(/[@#$%^&]/g) || []).length;
 
-  if (s1 + s2 + s3 + s4 !== length) {
-    isErr = true;
-    errors.push("Only ('@', '#', '$', '%', '^', '&') special characters allowed.");
-  }
+  if (s1 + s2 + s3 + s4 !== length)
+    return [true, "Only ('@', '#', '$', '%', '^', '&') special characters allowed."];
 
-  const message = errors.join('\n');
-
-  return [isErr, message];
+  return [false, ''];
 };
 
 export default validatePassword;
